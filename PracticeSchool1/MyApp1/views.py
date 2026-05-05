1ecec9b10dd0553b449e8144a085b172486a7c77
8001688 <8001688@schoolsnet.act.edu.au>
2026-05-05 10:17:54 +1000

    Added image and PDF downloader

--- PracticeSchool1/MyApp1/views.py
+++ PracticeSchool1/MyApp1/views.py
@@ -1,4 +1,5 @@
 import http
+from urllib import response
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime 
@@ -6,6 +7,18 @@ from .models import teacher
 from .forms import InputForm
 from django.contrib.auth.models import User
 from .forms import SignUpForm
+from pypdf import PdfWriter, PdfReader #Joining PDFs
+
+from reportlab.pdfgen import canvas #Generating PDfs
+
+from reportlab.platypus import Paragraph,Image,Table #Generating PDfs
+
+from django.http import FileResponse #Downloading files
+
+from django.contrib.staticfiles.storage import staticfiles_storage #Working with static files
+
+from io import BytesIO #Using Byte stream
+
 
 # Teachers #
 
@@ -46,7 +59,7 @@ def signup_view(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-            return redirect("index")
+            return redirect("home")
 
     else:
         form = SignUpForm()
@@ -56,4 +69,52 @@ def signup_view(request):
 # HomePage
 
 def home(request):
-    return render(request, "MyApp1/home.html")                
+    return render(request, "MyApp1/home.html")        
+
+# PDF
+
+def report(request):
+    pdf_file = staticfiles_storage.path("DS.pdf")
+
+    try: 
+        merger = PdfWriter()
+
+        input1 = PdfReader(generate_pdf())
+        input2 = PdfReader(pdf_file, "rb")
+
+        merger.append(input1)
+        merger.append(input2)
+
+        buffer = BytesIO()
+        merger.write(buffer)
+        buffer.seek(0)
+
+        response = FileResponse(buffer, as_attachment=True, filename="Attachment.pdf")
+
+    except FileNotFoundError:
+        response = FileResponse(generate_pdf(), as_attachment=True, filename="noAttachment.pdf")
+
+    return response 
+
+def generate_pdf():
+    
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    lines = [('Name', 'Teaching Area:')]
+
+    teachers = teacher.objects.all()
+
+    for teach in teachers: 
+        lines.append((teach.Name, teach.Area))
+    
+    table = Table(lines)
+    table.wrapOn(p, 300, 300)
+    table.drawOn(p, 10, 650)
+
+    p.showPage()
+    p.save()
+
+    buffer.seek(0)
+    return buffer 
+
--- PracticeSchool1/PracticeSchool1/urls.py
+++ PracticeSchool1/PracticeSchool1/urls.py
@@ -8,15 +8,18 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
    path('admin/', admin.site.urls),
    re_path(r'^home$', MyApp1.views.home, name='home'),
-   re_path(r'^$', MyApp1.views.index, name='index'),
+   re_path(r'^$', MyApp1.views.home, name='index'),
+   re_path(r'^$', MyApp1.views.index, name='test'),
+
   
    re_path(r'input', MyApp1.views.input_view, name='input'),
    path('delete/<int:teacher_id>/', MyApp1.views.delete_teacher, name='delete_teacher'), # delete teachers
    path('signup/', MyApp1.views.signup_view, name='signup'), #Signup Page
    path('login/', auth_views.LoginView.as_view(template_name='MyApp1/login.html'), name='login'),
-path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-
+   path("logout/", auth_views.LogoutView.as_view(), name="logout"),
    path('home/', MyApp1.views.home, name='home'),
+   path('report/', MyApp1.views.report, name='report'),
+   path('test/', MyApp1.views.index, name='test')
 
 
 ]
